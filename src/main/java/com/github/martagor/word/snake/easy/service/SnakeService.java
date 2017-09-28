328c8901b9f0732e66eb1be5823a4509481828ce
Marta <marta.gorlicka@gmail.com>
2017-09-28 20:30:55 +0200

    add findAll method

--- src/main/java/com/github/martagor/word/snake/easy/service/SnakeService.java
+++ src/main/java/com/github/martagor/word/snake/easy/service/SnakeService.java
@@ -1,6 +1,11 @@
 package com.github.martagor.word.snake.easy.service;
 
+import com.github.martagor.word.snake.easy.model.PieceOfSnake;
+
+import java.util.List;
+
 public interface SnakeService {
     void addWord(String word);
+    List<String> findAll();
 
 }
--- src/main/java/com/github/martagor/word/snake/easy/service/impl/SnakeServiceImpl.java
+++ src/main/java/com/github/martagor/word/snake/easy/service/impl/SnakeServiceImpl.java
@@ -4,17 +4,23 @@ package com.github.martagor.word.snake.easy.service.impl;
 import com.github.martagor.word.snake.easy.model.PieceOfSnake;
 import com.github.martagor.word.snake.easy.repository.SnakeRepository;
 import com.github.martagor.word.snake.easy.service.SnakeService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
 
 @Service
 public class SnakeServiceImpl implements SnakeService {
 
     @Autowired
     SnakeRepository snakeRepository;
-
 
     @Override
     public void addWord(String word) {
@@ -25,6 +31,14 @@ public class SnakeServiceImpl implements SnakeService {
             snake.setTime(LocalDateTime.now());
             snakeRepository.save(snake);
         }
+    }
+
+    @Override
+    public List<String> findAll() {
+        return snakeRepository.findAll().stream()
+                .sorted(comparing(PieceOfSnake::getTime))
+                .map(PieceOfSnake::getWord)
+                .collect(toList());
     }
 
 }
